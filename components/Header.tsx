90473cc856d96f9c3cfb9c98dd6af136cbf90d64
diebraga <diebraga0055@gmail.com>
2021-06-06 17:27:52 +0100

    header details

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,5 +1,5 @@
-import { Flex, Input, Text, Icon } from '@chakra-ui/react'
-import { RiSearchLine } from "react-icons/ri";
+import { Flex, Input, Text, Icon, HStack, Box, Avatar } from '@chakra-ui/react'
+import { RiNotificationLine, RiSearchLine, RiUserAddLine } from "react-icons/ri";
 
 export function Header() {
   return (
@@ -44,6 +44,35 @@ export function Header() {
           _placeholder={{ color: 'gray.400' }}
         />
         <Icon as={RiSearchLine} fontSize='20'/>
+      </Flex>
+
+      <Flex
+        align='center'
+        ml='auto'
+      >
+        <HStack
+          spacing='8'
+          mx='8'
+          pr='8'
+          py='1'
+          color='gray.300'
+          borderRightWidth={1}
+          borderColor='gray.700'
+        >
+          <Icon as={RiNotificationLine} fontSize='20'/>
+          <Icon as={RiUserAddLine} fontSize='20'/>
+        </HStack>
+
+        <Flex align='center'>
+          <Box mr='4' textAlign='right'>
+            <Text>Diego Braga</Text>
+            <Text fontSize='small' color='gray.300'>
+              diebraga.developer@gmail.com
+            </Text>
+          </Box>
+
+          <Avatar size='md' name='Diego Braga' src='' />
+        </Flex>
       </Flex>
     </Flex>
   )
